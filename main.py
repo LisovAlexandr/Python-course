565165ecde0a748b3560aba224cd56868c870aae
LisovAlexandr <79405023+LisovAlexandr@users.noreply.github.com>
2022-03-22 08:49:48 +0300

    File upload via browser

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-"""
-numbers1 = {2, 4, 3, 6}
-numbers2 = {3, 5, 4, 7}
-numbers3 = numbers1 | numbers2
-numbers3 = numbers1 & numbers2
-numbers3 = numbers1 - numbers2
-print(numbers3)
-"""
-
-
+print('Hi')
